8952d43060490c9876a84fc9be4939920cfad01d
misha825 <mikhailr26@ausdk12.org>
2024-08-23 16:48:52 +0000

    water

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,42 @@
+public class BodyOfWater {
+    private String name;
+    private int largestD;
+    private int averageD;
+    private boolean isSaltWater;
+
+    public BodyOfWater(){
+        largestD = 1000;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAverageD(){
+        return averageD;
+    }
+
+
+
+    public void setAverageD(int ld){
+        largestD = ld;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+
+    public BodyOfWater(String n, int ld, int ad, boolean sw){
+        name = n;
+        largestD = ld;
+        averageD = ad;
+        isSaltWater = sw;
+    }
+
+    public BodyOfWater(String n, int ad){
+        name = n;
+        averageD = ad;
+    }
+}
 
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,11 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Pacific Ocean", 1000);
+        b.setName("Pacific Ocean");
+        b.setAverageD(1000);
+        System.out.println(b.getName());
+        System.out.println(b.getAverageD());
     }
+
+    
 }
